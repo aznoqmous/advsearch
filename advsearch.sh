70440b16f5dd1bf2a84e68fab3b55828b75d7265
aznoqmous <paul.landreau931v@gmail.com>
2019-09-02 14:36:26 +0200

    added matching line number + colored string in output

--- advsearch.sh
+++ advsearch.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+SRC=${0%/*}
+source "$SRC/echolor.sh"
+
 file_pattern=$1
 string=$2
 dir=$3
@@ -13,7 +16,6 @@ then
   usage
 fi
 
-
 if [[ -z $dir ]]
 then
   dir=$(pwd)
@@ -22,8 +24,7 @@ files=$(locate "$dir/$file_pattern")
 
 files_count=$(echo "$files" | wc -l)
 files_count=$(($files_count-1))
-echo "Updating database..."
-updatedb
+
 echo "$files_count files to search"
 i=0
 matching=0
@@ -31,19 +32,23 @@ for file in $files
 do
   if [[ -f "$file" ]]
   then
-    if [[ -z $(cat "$file" | grep "$string") ]]
+    match=$(cat "$file" | grep "$string")
+    if [[ -z $match ]]
   	then
     		echo "Not found" > /dev/null
     	else
+
         echo "                                          "
-    		echo $file
+    		echolor cyan "$file"
         echo
-        cat "$file" | grep "$string"
+
+        match=$(cat "$file" | grep -n "$string" | sed "s/$string/{{$string}}/g")
+        echolor orange "$match"
         matching=$(($matching+1))
     fi
-    i=$(($i+1))
     printf "$i/$files_count files processed...\r"
+    i=$(($i+1))
   fi
 done
-
+echo ""
 echo "$matching files found containing '$string' out of $files_count files matching $file_pattern"
